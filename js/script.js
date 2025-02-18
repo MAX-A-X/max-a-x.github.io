30652e3a20c5e9fe3da913e44afd42333b56538f
MAX-A-X <9675036@qq.com>
2025-02-18 17:24:12 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -127,3 +127,21 @@ class ThemeManager {
 }
 
 new ThemeManager();
+
+
+const backToTop = document.querySelector('.back-to-top');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) {
+    backToTop.classList.add('visible');
+  } else {
+    backToTop.classList.remove('visible');
+  }
+});
+
+backToTop.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
